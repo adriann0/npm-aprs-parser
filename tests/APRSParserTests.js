8a798bfaf0d6ac2b7d7fd9be4eaa08e68a99ab2f
adriann0 <adriann0@users.noreply.github.com>
2017-02-09 14:14:49 +0100

    Refactoring - decision on type might require knowing whole message content

--- tests/APRSParserTests.js
+++ tests/APRSParserTests.js
@@ -20,6 +20,7 @@ describe('APRSParser', function () {
         var obj = parser.parse("SQ7PFS-15>APRS,TCPIP:");
         expect(obj).to.be.an.instanceOf(APRSMessage);
 
+        expect(obj).to.exist;
         expect(obj.from).to.eql(new Callsign("SQ7PFS-15"));
         expect(obj.to).to.eql(new Callsign("APRS"));
         expect(obj.digi).to.eql([new Callsign("TCPIP")]);
@@ -27,21 +28,32 @@ describe('APRSParser', function () {
 
     it('Not compressed position recognition', function () {
         var obj = parser.parse("SQ7PFS>APRS:!5214.93ND02106.32E&");
+
+        expect(obj).to.exist;
+        expect(obj.data).to.exist;
         expect(obj.data).to.be.an.instanceOf(Position);
     });
 
     it('Message recognition', function () {
         var obj = parser.parse("SQ7PFS>APRS::WU2Z     :Testing{003 ");
+
+        expect(obj).to.exist;
+        expect(obj.data).to.exist;
         expect(obj.data).to.be.an.instanceOf(Message);
     });
 
     it('Telemetry recognition', function () {
         var obj = parser.parse("SQ7PFS>APRS:T#005,199,000,255,073,123,01101001");
+
+        expect(obj).to.exist;
+        expect(obj.data).to.exist;
         expect(obj.data).to.be.an.instanceOf(Telemetry);
     });
 
     it('Unknown format recognition', function () {
         var obj = parser.parse("SQ7PFS>APRS:Z");
+
+        expect(obj).to.exist;
         expect(obj).to.be.an.instanceOf(APRSMessage);
         expect(obj.data).to.not.exist;
     });
